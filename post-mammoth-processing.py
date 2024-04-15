fb2e31fd9759c6748e064ca349a24a561dd78427
Digital-Grinnell <digital@grinnell.edu>
2024-04-15 10:26:56 -0500

    Commented out unused code

--- post-mammoth-processing.py
+++ post-mammoth-processing.py
@@ -329,96 +329,96 @@ def rootstalk_markdownify(filepath):
                 
 
 # --------------------------------------------------------------------------------
-def rootstalk_azure_media(year, term, filepath):
-  # ytmd = "{}-{}.md".format(year, term, year, term)
-  ytmd = filepath.replace(".html", ".md")
+# def rootstalk_azure_media(year, term, filepath):
+#   # ytmd = "{}-{}.md".format(year, term, year, term)
+#   ytmd = filepath.replace(".html", ".md")
 
-  # Open the issue's year-term.md file...
-  logging.info("Attempting to open markdown file: " + ytmd)
-  with open(ytmd, "r") as issue_md:
-    # azure_path = "{}-{}-azure.md".format(year, term)
-    azure_path = filepath.replace(".html", "-azure.md")
+#   # Open the issue's year-term.md file...
+#   logging.info("Attempting to open markdown file: " + ytmd)
+#   with open(ytmd, "r") as issue_md:
+#     # azure_path = "{}-{}-azure.md".format(year, term)
+#     azure_path = filepath.replace(".html", "-azure.md")
   
-    logging.info("Creating new Azure .md file at '{}'.".format(azure_path))
+#     logging.info("Creating new Azure .md file at '{}'.".format(azure_path))
 
-    # Open and write a new year-term-azure.md file...
-    with open(azure_path, "w") as azure_md:
-      lines = issue_md.readlines()
+#     # Open and write a new year-term-azure.md file...
+#     with open(azure_path, "w") as azure_md:
+#       lines = issue_md.readlines()
 
-      # Clean-up...
-      # - translate any year-term-web-resources folder references to new Azure format.
-      # - remove any line that entirely matches the pattern:  ^.+ | .+$
+#       # Clean-up...
+#       # - translate any year-term-web-resources folder references to new Azure format.
+#       # - remove any line that entirely matches the pattern:  ^.+ | .+$
 
-      for line in lines:
-        match_image = re.match(image_pattern, line)
-        match_header = re.match(header_pattern, line)
-        if match_image:  # transform image references
-          new_line = replacement.replace("xPIDx", match_image.group(1))
-          print(new_line, end='\n', file=azure_md)
-        elif not match_header:  # skip page headers
-          print(line, file=azure_md)  # write the line out
+#       for line in lines:
+#         match_image = re.match(image_pattern, line)
+#         match_header = re.match(header_pattern, line)
+#         if match_image:  # transform image references
+#           new_line = replacement.replace("xPIDx", match_image.group(1))
+#           print(new_line, end='\n', file=azure_md)
+#         elif not match_header:  # skip page headers
+#           print(line, file=azure_md)  # write the line out
 
-  # Now, remove all repeated blank lines (reduces whitespace)
-  with open(azure_path, "r+") as azure_md:
-    contents = azure_md.read( )
-    # stripped = re.sub(r'^$\n', '', contents, flags=re.MULTILINE)
-    stripped = re.sub(r'\n\s*\n', '\n\n', contents)
-    azure_md.seek(0)  # rewind the file
-    azure_md.writelines(stripped)  # write the stripped version
+#   # Now, remove all repeated blank lines (reduces whitespace)
+#   with open(azure_path, "r+") as azure_md:
+#     contents = azure_md.read( )
+#     # stripped = re.sub(r'^$\n', '', contents, flags=re.MULTILINE)
+#     stripped = re.sub(r'\n\s*\n', '\n\n', contents)
+#     azure_md.seek(0)  # rewind the file
+#     azure_md.writelines(stripped)  # write the stripped version
 
 
-# ----------------------------------------------------------------------------------
-def rootstalk_make_articles(year, term, filepath):
-  ytyml = filepath.replace(".html", ".yml")
+# # ----------------------------------------------------------------------------------
+# def rootstalk_make_articles(year, term, filepath):
+#   ytyml = filepath.replace(".html", ".yml")
   
-  # Look for a year-term.yml file...
-  if not os.path.exists(ytyml):
-    logging.error("ERROR: No {} YAML file found! You need to create this file if you wish to proceed with the {}-{} issue!".format(ytyml, year, term))
-  else:
-    logging.info("Processing the {} file.".format(ytyml))
+#   # Look for a year-term.yml file...
+#   if not os.path.exists(ytyml):
+#     logging.error("ERROR: No {} YAML file found! You need to create this file if you wish to proceed with the {}-{} issue!".format(ytyml, year, term))
+#   else:
+#     logging.info("Processing the {} file.".format(ytyml))
         
-    # Check for corresponding -azure.md file in the same directory
-    azure_md = filepath.replace(".html", "-azure.md") 
-    if not os.path.exists(azure_md):
-      logging.error(
-            "ERROR: No Azure-formatted markdown file '{}' found! You may need to run the 'rootstalk_azure_media' scripts before proceeding.".format(
-              azure_md))
+#     # Check for corresponding -azure.md file in the same directory
+#     azure_md = filepath.replace(".html", "-azure.md") 
+#     if not os.path.exists(azure_md):
+#       logging.error(
+#             "ERROR: No Azure-formatted markdown file '{}' found! You may need to run the 'rootstalk_azure_media' scripts before proceeding.".format(
+#               azure_md))
         
-    # Everything is in place, read the year-term.yml file...
-    with open(ytyml, "r") as stream:
-      try:
-        yml = yaml.safe_load(stream)
-      except yaml.YAMLError as exc:
-        sys.exit(exc)
+#     # Everything is in place, read the year-term.yml file...
+#     with open(ytyml, "r") as stream:
+#       try:
+#         yml = yaml.safe_load(stream)
+#       except yaml.YAMLError as exc:
+#         sys.exit(exc)
       
-      for key, value in yml.items():
-        logging.info("{}: {}".format(key, value))
+#       for key, value in yml.items():
+#         logging.info("{}: {}".format(key, value))
 
-      # Read each article name/index and create a new article_index.md file if one does not already exist
-      for name in yml["articles"]:
-        web_resources = '-web-resources/{}.md'.format(name)
-        md_path = filepath.replace(".html", web_resources)
-        logging.info("Creating article markdown file '{}'...".format(md_path))
-        if os.path.exists(md_path):
-          logging.warning(
-                "WARNING: Markdown file '{}' already exists and will not be replaced! Be sure to move or remove the existing file if you wish to generate a new copy.".format(
-                  md_path))
-        else:
-          with open(azure_md, "r") as md:
-            issue_md_content = md.read()
+#       # Read each article name/index and create a new article_index.md file if one does not already exist
+#       for name in yml["articles"]:
+#         web_resources = '-web-resources/{}.md'.format(name)
+#         md_path = filepath.replace(".html", web_resources)
+#         logging.info("Creating article markdown file '{}'...".format(md_path))
+#         if os.path.exists(md_path):
+#           logging.warning(
+#                 "WARNING: Markdown file '{}' already exists and will not be replaced! Be sure to move or remove the existing file if you wish to generate a new copy.".format(
+#                   md_path))
+#         else:
+#           with open(azure_md, "r") as md:
+#             issue_md_content = md.read()
                 
-            # Customize the front matter before inserting it...
-            fm = frontmatter.replace("index: ", "index: {}".format(name))
-            fm = fm.replace("articleIndex: ", "articleIndex: {}".format(aIndex))
-            fm = fm.replace("azure_dir: ", "azure_dir: rootstalk-{}-{}".format(year, term))
-            fm = fm.replace("date: ", "date: '{}'".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S')))
+#             # Customize the front matter before inserting it...
+#             fm = frontmatter.replace("index: ", "index: {}".format(name))
+#             fm = fm.replace("articleIndex: ", "articleIndex: {}".format(aIndex))
+#             fm = fm.replace("azure_dir: ", "azure_dir: rootstalk-{}-{}".format(year, term))
+#             fm = fm.replace("date: ", "date: '{}'".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S')))
             
-            aIndex += 1
+#             aIndex += 1
             
-            # Write the front matter and content to the article.md file
-            with open(md_path, "w") as article_md:
-              print(fm, file=article_md)
-              print(issue_md_content, file=article_md)
+#             # Write the front matter and content to the article.md file
+#             with open(md_path, "w") as article_md:
+#               print(fm, file=article_md)
+#               print(issue_md_content, file=article_md)
 
 
 # Main...
